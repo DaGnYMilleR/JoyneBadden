1a788ad2c4b5fc217b56810af143b35248ca91a2
lunev.d <lunev.d@kontur.ru>
2024-04-27 20:52:26 +0500

    Swap buckets * button doesn't works * swap executes immediately

--- Assets/scripts/bucket/BucketScript.cs
+++ Assets/scripts/bucket/BucketScript.cs
@@ -11,9 +11,21 @@ public class BucketScript : MonoBehaviour
     {
     }
 
-    public void Move(Vector3 target) => 
-        transform.position = Vector3.Lerp(transform.position, target, _damping * Time.deltaTime);
+    public void Move(Vector3 target)
+    {
+        var i = 0f;
+        while (i < 1.0) {
+            i += Time.deltaTime * _damping;
+            transform.position = Vector3.Lerp(transform.position, target, i);
+        }
+    }
 
-    public void MoveParabolic(Vector3 target) =>
-        transform.position = Vector3.Lerp(transform.position, target, _damping * Time.deltaTime);
+    public void MoveParabolic(Vector3 target)
+    {
+        var i = 0f;
+        while (i < 1.0) {
+            i += Time.deltaTime * _damping;
+            transform.position = Vector3.Slerp(transform.position, target, i);
+        }
+    }
 }
