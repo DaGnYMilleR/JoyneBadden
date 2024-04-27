e4002ca255bb8a3d2aff04f3409353fb2e5c55fe
S4ske <sany1111.ru@mail.ru>
2024-04-27 23:29:15 +0500

    Add first scene

---
+++ Assets/scripts/SpeakingBadden.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakingBadden : MonoBehaviour
+{
+    public bool isSpeaking;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
