9e9ee5c304f3b34555aa1bc9434be45a9c937c0d
nasimkrut <nasimkrut@gmail.com>
2024-04-27 22:16:23 +0500

    add some scene

--- Assets/scripts/player/player.cs
+++ Assets/scripts/player/player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
@@ -8,7 +6,8 @@ public class player : MonoBehaviour
 
     public static Transform Instance { get; }
 
-    
+    private Animator animator;
+
     private Vector2 movement;
     private Rigidbody2D rb;
     private bool facingRight = true;
@@ -17,6 +16,7 @@ public class player : MonoBehaviour
     
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -25,6 +25,9 @@ public class player : MonoBehaviour
         movement = new Vector2(
                     (Input.GetKey(KeyCode.A) ? -1 : 0) + (Input.GetKey(KeyCode.D) ? 1 : 0),
                     (Input.GetKey(KeyCode.S) ? -1 : 0) + (Input.GetKey(KeyCode.W) ? 1 : 0));
+        
+        animator.SetBool("isRunning", !movement.Equals(Vector2.zero));
+
         
         if (facingRight && movement.x > 1e-8 || !facingRight && movement.x < -1e-8)
             Flip();
@@ -40,5 +43,7 @@ public class player : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = movement.normalized * moveSpeed;
+        if (rb.velocity.x > 0)
+            Debug.Log(rb.velocity.x);
     }
 }
