c84e7572e0fc2e637b775afa5e11ec003b78bc46
lunev.d <lunev.d@kontur.ru>
2024-04-28 02:22:09 +0500

    show key in baby-badden location only after mini game

--- Assets/scripts/Key.cs
+++ Assets/scripts/Key.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    private static bool isInteractable = false; 
+    
+    private void Start()
+    {
+        if (!isInteractable)
+            gameObject.SetActive(false);
+    }
+
+    public void ShowKey()
+    {
+        isInteractable = true;
+        gameObject.SetActive(true);
+    }
+
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && Input.GetKey(KeyCode.E))
+        if (other.CompareTag("Player") && Input.GetKey(KeyCode.E) && isInteractable)
         {
             other.GetComponent<player>().hasKey = true;
             Destroy(gameObject);
